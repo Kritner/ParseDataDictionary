bc84d70ed1647c3425f3816d7633d101e844fe24
kritner <kritner@gmail.com>
2016-04-12 07:21:39 -0400

    LoadParseExportDataDictionaryService and SQLScriptFileWriterService tests

---
+++ ParseDataDictionary.Business.Tests/Services/LoadParseExportDataDictionaryServiceTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using ClosedXML.Excel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ParseDataDictionary.Business.Interfaces;
+using ParseDataDictionary.Business.Models;
+using ParseDataDictionary.Business.Services;
+
+namespace ParseDataDictionary.Business.Tests.Services
+{
+
+    /// <summary>
+    /// Tests for LoadParseExportDataDictionaryService
+    /// </summary>
+    [TestClass]
+    public class LoadParseExportDataDictionaryServiceTests
+    {
+
+        #region Private
+        Mock<ILoadExcelFile> _mockILoadExcelFile;
+        Mock<IExcelDataDictionaryParser> _mockIExcelDataDictionaryParser;
+        Mock<IGenerateSqlScriptsForExtendedProperties> _mockIGenerateSqlScriptsForExtendedProperties;
+        Mock<IFileWriter> _mockIFileWriter;
+        LoadParseExportDataDictionaryService _biz;
+        #endregion Private
+
+        #region Setup
+        /// <summary>
+        /// Test initliazer
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockILoadExcelFile = new Mock<ILoadExcelFile>();
+            _mockIExcelDataDictionaryParser = new Mock<IExcelDataDictionaryParser>();
+            _mockIGenerateSqlScriptsForExtendedProperties = new Mock<IGenerateSqlScriptsForExtendedProperties>();
+            _mockIFileWriter = new Mock<IFileWriter>();
+
+            _biz = new LoadParseExportDataDictionaryService(
+                _mockILoadExcelFile.Object,
+                _mockIExcelDataDictionaryParser.Object,
+                _mockIGenerateSqlScriptsForExtendedProperties.Object,
+                _mockIFileWriter.Object
+            );
+        }
+        #endregion Setup
+
+        #region Public methods/tests
+        /// <summary>
+        /// Tests successful construction
+        /// </summary>
+        [TestMethod]
+        public void LoadParseExportDataDictionaryService_ctor_ConstructedProperly()
+        {
+            // Assert
+            Assert.IsInstanceOfType(_biz, typeof(LoadParseExportDataDictionaryService));
+        }
+
+        /// <summary>
+        /// ArgumentNullException is thrown with no provided ILoadExcelFile
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LoadParseExportDataDictionaryService_ctor_ArgumentNullExceptionThrownWhenILoadExcelFileNotProvided()
+        {
+            // Arrange / Act / Assert
+            _biz = new LoadParseExportDataDictionaryService(
+                null,
+                _mockIExcelDataDictionaryParser.Object,
+                _mockIGenerateSqlScriptsForExtendedProperties.Object,
+                _mockIFileWriter.Object
+            );
+        }
+
+        /// <summary>
+        /// ArgumentNullException is thrown with no provided IExcelDataDictionaryParser
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LoadParseExportDataDictionaryService_ctor_ArgumentNullExceptionThrownWhenIExcelDataDictionaryParserNotProvided()
+        {
+            // Arrange / Act / Assert
+            _biz = new LoadParseExportDataDictionaryService(
+                _mockILoadExcelFile.Object,
+                null,
+                _mockIGenerateSqlScriptsForExtendedProperties.Object,
+                _mockIFileWriter.Object
+            );
+        }
+
+        /// <summary>
+        /// ArgumentNullException is thrown with no provided IGenerateSqlScriptsForExtendedProperties
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LoadParseExportDataDictionaryService_ctor_ArgumentNullExceptionThrownWhenIGenerateSqlScriptsForExtendedPropertiesNotProvided()
+        {
+            // Arrange / Act / Assert
+            _biz = new LoadParseExportDataDictionaryService(
+                _mockILoadExcelFile.Object,
+                _mockIExcelDataDictionaryParser.Object,
+                null,
+                _mockIFileWriter.Object
+            );
+        }
+
+        /// <summary>
+        /// ArgumentNullException is thrown with no provided IFileWriter
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LoadParseExportDataDictionaryService_ctor_ArgumentNullExceptionThrownWhenIFileWriterNotProvided()
+        {
+            // Arrange / Act / Assert
+            _biz = new LoadParseExportDataDictionaryService(
+                _mockILoadExcelFile.Object,
+                _mockIExcelDataDictionaryParser.Object,
+                _mockIGenerateSqlScriptsForExtendedProperties.Object,
+                null
+            );
+        }
+
+        /// <summary>
+        /// Execute calls all dependencies
+        /// </summary>
+        [TestMethod]
+        public void LoadParseExportDataDictionaryService_Execute_CallsDependencies()
+        {
+            // Arrange
+            _mockILoadExcelFile
+                .Setup(s => s.Execute())
+                .Returns(It.IsAny<XLWorkbook>());
+            _mockIExcelDataDictionaryParser
+                .Setup(s => s.ParseDocumentIntoModel(It.IsAny<XLWorkbook>()))
+                .Returns(It.IsAny<IEnumerable<Table>>());
+            _mockIGenerateSqlScriptsForExtendedProperties
+                .Setup(s => s.GetSqlScripts(It.IsAny<IEnumerable<Table>>()))
+                .Returns(new List<string>());
+
+            // Act
+            var results = _biz.Execute();
+
+            // Assert
+            Assert.IsInstanceOfType(results, typeof(List<string>), "results type");
+            _mockILoadExcelFile.Verify(v => v.Execute(), Times.Once, "ILoadExcelFile Execute");
+            _mockIExcelDataDictionaryParser.Verify(v => v.ParseDocumentIntoModel(It.IsAny<XLWorkbook>()), Times.Once, "IExcelDataDictionaryParser ParseDocumentIntoModel");
+            _mockIGenerateSqlScriptsForExtendedProperties.Verify(v => v.GetSqlScripts(It.IsAny<IEnumerable<Table>>()), Times.Once, "IGenerateSqlScriptsForExtendedProperties GetSqlScripts");
+            _mockIFileWriter.Verify(v => v.WriteFile(It.IsAny<string>(), It.IsAny<IEnumerable<string>>()), Times.Once, "IFileWriter WriteFile");
+        }
+        #endregion Public methods/tests
+    }
+}
---
+++ ParseDataDictionary.Business.Tests/Services/SQLScriptFileWriterServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParseDataDictionary.Business.Services;
+
+namespace ParseDataDictionary.Business.Tests.Services
+{
+
+    /// <summary>
+    /// Tests for SQLScriptFileWriterService
+    /// </summary>
+    [TestClass]
+    public class SQLScriptFileWriterServiceTests
+    {
+        #region const
+        const string _TEST_STRING = "this is a test";
+        #endregion const
+
+        #region Private
+        SQLScriptFileWriterService _biz;
+        #endregion Private
+
+        #region Setup
+        [TestInitialize]
+        public void Setup()
+        {
+            _biz = new SQLScriptFileWriterService();
+        }
+        #endregion Setup
+
+        #region Public methods/tests
+        /// <summary>
+        /// WriteFile creates a file with the specified filename, and populates it with the data in the list of string.
+        /// </summary>
+        [TestMethod]
+        public void SQLScriptFileWriterService_WriteFile_CreatesFile()
+        {
+            // Arrange
+            Guid fileGuid = Guid.NewGuid();
+            string fileName = string.Format("{0}.txt", fileGuid);
+            List<string> list = new List<string>();
+            list.Add(_TEST_STRING);
+            List<string> textFileList = new List<string>();
+            string line;
+
+            // Act
+            _biz.WriteFile(fileName, list);
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textFileList.Add(line);
+                }
+            }
+
+            // Assert
+            Assert.IsTrue(File.Exists(fileName), "File exists");
+            Assert.AreEqual(list.Count, textFileList.Count, "File line count");
+            Assert.AreEqual(list.First(), textFileList.First(), "File list contents");            
+            
+            // Cleanup
+            File.Delete(fileName);
+        }
+        #endregion Public methods/tests
+    }
+}
